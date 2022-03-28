935853f5b00aabf6ad2c71633ec05182a404cfc5
Yuval Datner <contact@datner.me>
2022-03-28 18:50:19 +0300

    wip

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -5,10 +5,18 @@ const withNx = require('@nrwl/next/plugins/with-nx');
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  reactStrictMode: true,
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
+  },
+  images: {
+    domains: ['images.unsplash.com', 'res.cloudinary.com', 'localhost'],
+  },
+  i18n: {
+    locales: ['en-US', 'he'],
+    defaultLocale: 'en-US',
   },
 };
 

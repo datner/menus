d3090e121a8cdba4f332efa6fc197690db4251a4
Yuval Datner <contact@datner.me>
2022-04-14 05:07:22 +0300

    feat(ui): [Grid] add Spacer

--- libs/ui/src/lib/Grid/index.ts
+++ libs/ui/src/lib/Grid/index.ts
@@ -1,6 +1,8 @@
 import { ForwardedGrid as _Grid } from './Grid';
 import { ForwardedItem } from './Item';
+import { ForwardedSpacer } from './Spacer';
 
 export const Grid = Object.assign(_Grid, {
   Item: ForwardedItem,
+  Spacer: ForwardedSpacer,
 });
